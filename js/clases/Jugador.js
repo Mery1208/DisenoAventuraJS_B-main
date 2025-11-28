8373431c155a080039b3c101ac0e5c3e34e942c4
Mery <mceballosm04@iesalbarregas.es>
2025-11-28 02:15:46 +0100

    jugador y utils

--- js/clases/Jugador.js
+++ js/clases/Jugador.js
@@ -1,4 +1,47 @@
 //-----------------------------------------------------------
-//CLASE ENEMIGO
-// Aquí hice la base creando a los enemigos del juego para usarlo después 
+//CLASE JUGADOR
+// Aquí hice la base creando al cazador (jugador principal)
 //-------------------------------------------------------------
+
+export class Jugador {
+    //añado constructor con sus datos 
+     constructo({nombre, avatar, vida= 100, puntos = 100 }) {
+        this.nombre = nombre;
+        this.avatar = avatar;
+        this.vidaBase = vida;
+        this.puntos = puntos;
+        this.inventario = []; //es el inventario donde estará los productos que compre luego
+    }
+    
+    //Objetivo: Intento añadir un objeto a mi mochila (inventario).
+    // Devuelve: 'true' si lo puse, 'false' si ya está lleno.
+    añadirObjeto(producto) {
+    }
+
+
+    retirarObjeto(nombreProducto) {
+
+    }
+
+    sumarPuntos() {
+
+    }
+
+    getAtaqueTotal() {
+
+    }
+
+     getDefensaTotal() {
+        
+    }
+
+     getVidaTotal() {
+        
+    }
+
+
+
+
+
+
+}
